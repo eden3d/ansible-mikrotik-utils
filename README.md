aeb0cc3a3daafc79f08f8dd92b68423fbd33e037
Hugo GEOFFROY <pistache@newstash.usr.ed3n.net>
2016-06-13 17:49:37 +0200

    Packaging changes

---
+++ README.md
@@ -0,0 +1,24 @@
+ansible-mikrotik-utils
+======================
+
+This library provides a way to represent Mikrotik devices configuration and
+determine the changes required to get the device to another configuration. This
+makes it possible to properly manage Mikrotik devices from configuration
+management tools, such as Ansible.
+
+Documentation
+-------------
+
+TODO
+
+License
+-------
+
+See `LICENSE.txt`.
+
+Unit tests
+----------
+
+There is currently only a single unit test located in `src/test`.
+
+
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 # Root package meta-data
 # ----------------------
 _name = 'ansible-mikrotik-utils'
-_project = "ansible-mikrotik-utils"
 _version = '0.0.1'
+_keywords = ['mikrotik', 'ansible', 'configuration']
 
 # Pwackages
 # --------
@@ -28,7 +28,8 @@ _description = (
 )
 _author = 'EDEN 3D Engineering'
 _author_email = 'contact@eden-3d.org'
-_url = 'https://www.eden-3d.org'
+_url = 'https://github.com/eden3d/ansible-mikrotik-utils'
+_download_url = 'https://github.com/eden3d/ansible-mikrotik-utils/zipball/master'
 _copyright = '2016, EDEN 3D Engineering'
 _licence = 'GNU General Public Licence V3.0'
 _classifiers = [
@@ -55,12 +56,14 @@ _tests_require = [
 setup(
     name=_name,
     version=_version,
+    keywords=_keywords,
     packages=_packages,
     package_dir=_package_dir,
     description=_description,
     author=_author,
     author_email=_author_email,
     url=_url,
+    download_url=_download_url,
     license=_licence,
     classifiers=_classifiers,
     setup_requires=_setup_requires,
